3e384b1e893d3b5542dc7e7c64daa71e06d1c4bb
Igor <igorgonribsilva@gmail.com>
2020-10-26 20:03:59 -0300

    Commit inicial

---
+++ email-worker-compose/worker/worker.py
@@ -0,0 +1,17 @@
+import redis
+import json
+from time import sleep
+import os
+from random import randint
+
+if __name__ == '__main__':
+    redis_host = os.getenv('REDIS_HOST', 'queue')
+    print('Aguardando recebimento de mensagens...')
+    r = redis.Redis(host=redis_host, port=6379, db=0)
+    
+    while True:
+        mensagem = json.loads(r.blpop('sender')[1])
+        # Simulando envio de e-mail...
+        print('Mandando a mensagem:', mensagem['assunto'])
+        sleep(randint(15, 45))
+        print('Mensagem', mensagem['assunto'], 'enviada')
